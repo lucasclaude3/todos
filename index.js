9b271e3f56c82bbada0bd5a4fc76f11876b1e95a
kahlua <lucasclaude3@gmail.com>
2019-06-03 13:36:17 +0200

    Add menu labels

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let mainMenu;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -11,4 +12,18 @@ app.on('ready', () => {
     },
   });
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const menuTemplate = [
+  {
+    label: '',
+  },
+  {
+    label: 'File',
+    submenu: [
+      { label: 'New Todo'}
+    ]
+  }
+];
